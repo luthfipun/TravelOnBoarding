06084fcaf67a19aa3e8ce94f1e33ebd775985628
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-03-02 23:14:16 +0700

    Initial Commit

---
+++ TravelOnBoarding/TravelOnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelOnBoardingApp.swift
+//  TravelOnBoarding
+//
+//  Created by Luthfi Abdul Azis on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelOnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
